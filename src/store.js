7d6f2b7cf39e59da9e82827df04e1bf15d56a46b
Emmanuel B <blancocemmanuel@gmail.com>
2026-03-23 12:15:48 +1100

    commit

--- src/store.js
+++ src/store.js
@@ -3,10 +3,15 @@ import articlePreviewsReducer from './features/articlePreviews/articlePreviewsSl
 import currentArticleReducer from './features/currentArticle/currentArticleSlice';
 import commentsReducer from './features/comments/commentsSlice';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     articlePreviews: articlePreviewsReducer,
     currentArticle: currentArticleReducer,
     comments: commentsReducer
   },
 });
+
+
+
+
+export default store;
